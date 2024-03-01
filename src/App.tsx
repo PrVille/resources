a045e4cade45b2ef65ca2ae4708c9d900ec7eb9d
PrVille <ville.prami@me.com>
2024-03-01 23:25:42 +0200

    Vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import { GlobalStyles } from "./styles/global"
+import { Analytics } from "@vercel/analytics/react"
 import Home from "./pages/home"
 
 const StyledApp = styled.main``
@@ -7,6 +8,7 @@ const StyledApp = styled.main``
 const App = () => {
   return (
     <>
+      <Analytics />
       <GlobalStyles />
       <StyledApp>
         <Home />
